b53ce82b9b3d60d8282181709ebb2a3a044315c6
Zoltan Kauker <zoltan.kauker@doctusoft.com>
2018-01-08 15:35:08 +0100

    MQTT and TLS handshake examples

--- common/WiFi/Inc/es_wifi_io.h
+++ common/WiFi/Inc/es_wifi_io.h
@@ -1,8 +1,8 @@
 /**
   ******************************************************************************
-  * @file    Templates/Inc/main.h 
+  * @file    es_wifi_io.h
   * @author  MCD Application Team
-  * @brief   Header for main.c module
+  * @brief   This file contains the functions prototypes for es_wifi IO operations.
   ******************************************************************************
   * @attention
   *
@@ -32,24 +32,53 @@
   *
   ******************************************************************************
   */
-  
-/* Define to prevent recursive inclusion -------------------------------------*/
-#ifndef __MAIN_H
-#define __MAIN_H
+#ifndef __WIFI_IO__
+#define __WIFI_IO__
 
-#define STM32
-#define WOLFSSL_USER_SETTINGS
+#ifdef __cplusplus
+ extern "C" {
+#endif
 
 /* Includes ------------------------------------------------------------------*/
 #include "stm32l4xx_hal.h"
-#include "stm32l475e_iot01.h"
-#include "wifi.h"
 
-/* Exported types ------------------------------------------------------------*/
 /* Exported constants --------------------------------------------------------*/
+
 /* Exported macro ------------------------------------------------------------*/
-/* Exported functions ------------------------------------------------------- */
+#define WIFI_RESET_MODULE()                do{\
+                                            HAL_GPIO_WritePin(GPIOE, GPIO_PIN_8, GPIO_PIN_RESET );\
+                                            HAL_Delay(10);\
+                                            HAL_GPIO_WritePin( GPIOE, GPIO_PIN_8, GPIO_PIN_SET );\
+                                            HAL_Delay(500);\
+                                             }while(0);
 
-#endif /* __MAIN_H */
 
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
+#define WIFI_ENABLE_NSS()                  do{ \
+                                             HAL_GPIO_WritePin( GPIOE, GPIO_PIN_0, GPIO_PIN_RESET );\
+                                             HAL_Delay(10);\
+                                             }while(0);
+
+#define WIFI_DISABLE_NSS()                 do{ \
+                                             HAL_GPIO_WritePin( GPIOE, GPIO_PIN_0, GPIO_PIN_SET );\
+                                             HAL_Delay(10);\
+                                             }while(0);
+
+#define WIFI_IS_CMDDATA_READY()            (HAL_GPIO_ReadPin(GPIOE, GPIO_PIN_1) == GPIO_PIN_SET)
+
+    
+
+/* Exported functions ------------------------------------------------------- */ 
+void    SPI_WIFI_MspInit(SPI_HandleTypeDef* hspi);
+int8_t  SPI_WIFI_DeInit(void);
+int8_t  SPI_WIFI_Init(void);
+int16_t SPI_WIFI_ReceiveData(uint8_t *pData, uint16_t len, uint32_t timeout);
+int16_t SPI_WIFI_SendData( uint8_t *pData, uint16_t len, uint32_t timeout);
+void    SPI_WIFI_Delay(uint32_t Delay);
+    
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __WIFI_IO__ */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/    
