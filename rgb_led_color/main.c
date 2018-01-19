6d35b348fa1efd93a0c8e1bc4c713e01a3863a7a
geobalazs <balazskissgeo@gmail.com>
2018-01-19 16:43:05 +0100

    function return red, green, blue individually implemented

--- rgb_led_color/main.c
+++ rgb_led_color/main.c
@@ -1,18 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void json_hexa_for_rgbled(char led_color_hexa[]);
+//void json_hexa_for_rgbled(char led_color_hexa[]);
+
+int red_led(char led_color_hex[]);
+int green_led(char led_color_hex[]);
+int blue_led(char led_color_hex[]);
 
 int main()
 {
 
     char led_color[] = "FF80FF";
 
-    json_hexa_for_rgbled(led_color);
+    printf("red: %d" , red_led(led_color));
+    printf("green: %d" , green_led(led_color));
+    printf("blue: %d" , blue_led(led_color));
+
+//    json_hexa_for_rgbled(led_color);
 
     return 0;
 }
+int red_led(char led_color_hex[]){
 
+    int red = 0;
+    char red_text[2];
+    red_text[0] = led_color_hex[0];
+    red_text[1] = led_color_hex[1];
+    red = strtol(red_text , NULL , 16);
+
+    return red;
+}
+int green_led(char led_color_hex[]){
+
+    int green = 0;
+    char green_text[2];
+    green_text[0] = led_color_hex[2];
+    green_text[1] = led_color_hex[3];
+    green = strtol(green_text , NULL , 16);
+
+    return green;
+}
+int blue_led(char led_color_hex[]){
+
+    int blue = 0;
+    char blue_text[2];
+    blue_text[0] = led_color_hex[4];
+    blue_text[1] = led_color_hex[5];
+    blue = strtol(blue_text , NULL , 16);
+
+    return blue;
+}
+
+/*
 void json_hexa_for_rgbled(char led_color_hexa[]){
     //char led_color[] = led_color_hexa;
 
@@ -45,3 +84,4 @@ void json_hexa_for_rgbled(char led_color_hexa[]){
     printf(" hexablue: %d" , blue);
     printf(" hexagreen: %d" , green);
 }
+*/
